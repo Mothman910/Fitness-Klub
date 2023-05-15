56c74468ea39ea94f2d76b2d79a1f07b76473908
Adam Fijałkowski <fijalkowski.adam@outlook.com>
2023-05-15 22:18:27 +0200

    behavior of menu

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,43 @@ document
   .addEventListener("mouseleave", () => {
     document.getElementById("left-navbar-hidden").classList.remove("nav-hover");
   });
+// Obliczanie wysokości lewego menu
+let navHeight =
+  document.getElementById("search").getBoundingClientRect().bottom > 0
+    ? document.getElementById("search").getBoundingClientRect().bottom
+    : 0;
+document.getElementById(
+  "left-navbar-hidden"
+).style.height = `calc(100% - ${navHeight}px)`;
+
+addEventListener("resize", () => {
+  if (innerWidth > 576) {
+    document.getElementById("search").classList.remove("sticky-top");
+    document
+      .getElementsByClassName("main-nav")[0]
+      .classList.remove("position-relative");
+    document.getElementById("search").classList.add("position-fixed");
+  } else {
+    document.getElementById("search").classList.add("sticky-top");
+    document
+      .getElementsByClassName("main-nav")[0]
+      .classList.add("position-relative");
+    document.getElementById("search").classList.remove("position-fixed");
+  }
+  navHeight =
+    document.getElementById("search").getBoundingClientRect().bottom > 0
+      ? document.getElementById("search").getBoundingClientRect().bottom
+      : 0;
+  document.getElementById(
+    "left-navbar-hidden"
+  ).style.height = `calc(100% - ${navHeight}px)`;
+});
+addEventListener("scroll", () => {
+  navHeight =
+    document.getElementById("search").getBoundingClientRect().bottom > 0
+      ? document.getElementById("search").getBoundingClientRect().bottom
+      : 0;
+  document.getElementById(
+    "left-navbar-hidden"
+  ).style.height = `calc(100% - ${navHeight}px)`;
+});
