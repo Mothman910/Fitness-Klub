f44d7d8146beb519cd3869517ade06304512ea38
Adam Fijałkowski <fijalkowski.adam@outlook.com>
2023-05-15 22:24:12 +0200

    left nav

--- src/js/main.js
+++ src/js/main.js
@@ -18,7 +18,20 @@ let navHeight =
 document.getElementById(
   "left-navbar-hidden"
 ).style.height = `calc(100% - ${navHeight}px)`;
-
+// Zachowanie głównego menu
+if (innerWidth > 576) {
+  document.getElementById("search").classList.remove("sticky-top");
+  document
+    .getElementsByClassName("main-nav")[0]
+    .classList.remove("position-relative");
+  document.getElementById("search").classList.add("position-fixed");
+} else {
+  document.getElementById("search").classList.add("sticky-top");
+  document
+    .getElementsByClassName("main-nav")[0]
+    .classList.add("position-relative");
+  document.getElementById("search").classList.remove("position-fixed");
+}
 addEventListener("resize", () => {
   if (innerWidth > 576) {
     document.getElementById("search").classList.remove("sticky-top");
